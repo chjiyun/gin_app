65550a20cea37cc8f6c7c7e49214d72a2fd24e5b
chjiyun <lijiyun615@163.com>
2019-09-29 19:31:36 +0800

    sumit

--- api/index.go
+++ api/index.go
@@ -1,19 +1,20 @@
 package api
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
+// Index 测试语法的 API
 func Index(c *gin.Context) {
 	// 你也可以使用一个结构体
 	var msg struct {
-		Name    string `json:"user"`
-		Message string
-		Number  int
+		Result string
+		Number string
 	}
-	msg.Name = "Lena"
-	msg.Message = "hey, Hello World!"
-	msg.Number = http.StatusOK
+	x := 12345
+	msg.Result = "test Float"
+	msg.Number = fmt.Sprintf("x=%d", x)
 	c.JSON(http.StatusOK, msg)
 }

2a01c49a45d58a0932d016229366fe36af15d55a
chjiyun <lijiyun615@163.com>
2021-04-03 21:10:57 +0800

    一键部署脚本

---
+++ build.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+pwd=`pwd`
+projectName=`basename $pwd`
+# 编译的分支
+branch="master"
+# 编译后的输出文件名称，赋值当前项目文件名
+targetFile=projectName
+# 编译的包名
+buildPkg="main.go"
+# 编译结果
+buildResult=""
+
+pidFile="./pid.txt"
+
+today="$(date "+%Y_%m_%d")"
+# 日志存放路径
+logDir="/root/logs/${projectName}"
+info_log="${logDir}/info_${today}.log"
+error_log="${logDir}/error_${today}.log"
+
+echo "项目路径：${pwd}"
+
+if [ -n "$1" ]; then
+  branch="$1"
+  echo "Switch branch to ${branch}"
+else
+  echo "Building Branch: ${branch}"
+fi
+
+git checkout "$branch"
+git pull
+
+if [ -f $pidFile ]; then
+  pid=$(cat $pidFile)
+  echo "Prepare to kill the process: ${pid}"
+  kill -9 $pid
+fi
+
+buildResult=`go build -o "${targetFile}" "$buildPkg"`
+
+if [ -z "$buildResult" ]; then
+chmod 773 ${targetFile}
+echo "build success, filename：${targetFile}"
+else
+echo "build error $buildResult"
+exit
+fi
+
+nohup ${targetFile} 1>${info_log} 2>${error_log} & echo $! > $pidFile
+
+echo "deploy success"

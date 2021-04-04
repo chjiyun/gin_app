97e3b18fd20296de5b7354260aabfc08c5c269c6
chjiyun <lijiyun615@163.com>
2021-04-04 17:22:35 +0800

    清除半年前的日志文件

--- build.sh
+++ build.sh
@@ -13,14 +13,53 @@ buildResult=""
 
 pidFile="pid.txt"
 
-today="$(date "+%Y_%m_%d")"
+today=$(date "+%Y_%m_%d")
 # 日志存放路径
 logDir="/root/logs/${projectName}"
-info_log="${logDir}/info_${today}.log"
-error_log="${logDir}/error_${today}.log"
+info_log="${logDir}/info.${today}.log"
+error_log="${logDir}/error.${today}.log"
 
+if [ -f $pidFile ]; then
+  # pid=$(cat $pidFile)
+  pid=$(<$pidFile)
+  echo "Prepare to kill the process: ${pid}"
+  kill -9 $pid
+fi
 
 echo "项目路径：${pwd}"
+echo "start clean log file"
+
+if [ ! -d "$logDir" ]; then
+  mkdir "$logDir"
+fi
+
+# 找到半年前的日志文件
+m=$(date -d "-6 months" "+%s")
+
+echo "半年前的日期：$(date -d @${m} "+%Y-%m-%d")"
+
+index=1
+f=`ls ${logDir} -1 -c`
+
+for name in $f
+do
+  # echo "日志${index}：$name"
+  dateStr=$(echo ${name} | grep -Eo "[[:digit:]]{4}-[[:digit:]]{2}-[[:digit:]]{2}")
+  # echo $dateStr
+  # 判断是否有效
+  if date -d ${dateStr} > /dev/null 2>&1; then
+    t1=`date -d "$dateStr" +%s`
+
+    if [ $t1 -lt $m ]; then
+      echo ">>> delete file: ${name}"
+      rm -rf "${logDir}/${name}"
+    fi
+
+  fi
+  let index++
+done
+
+echo "complete the clean"
 
 if [ -n "$1" ]; then
   branch="$1"
@@ -32,26 +71,17 @@ fi
 git checkout "$branch"
 git pull
 
-if [ -f $pidFile ]; then
-  # pid=$(cat $pidFile)
-  pid=$(<$pidFile)
-  echo "Prepare to kill the process: ${pid}"
-  kill -9 $pid
-fi
 
 buildResult=`go build -o "${targetFile}" "$buildPkg"`
 
 if [ -z "$buildResult" ]; then
-chmod 773 ${targetFile}
-echo "build success, filename：${targetFile}"
+  chmod 773 ${targetFile}
+  echo "build success, filename：${targetFile}"
 else
-echo "build error $buildResult"
-exit
+  echo "build error $buildResult"
+  exit
 fi
 
-if [ ! -d "$logDir" ]; then
-  mkdir "$logDir"
-fi
 
 if [ ! -f "$info_log" ]; then
   touch "$info_log"

e302400c52c155da31f7fed1a2e4c6ea9a179ba4
chjiyun <lijiyun615@163.com>
2022-01-05 17:21:29 +0800

    fix

--- build.sh
+++ build.sh
@@ -78,7 +78,7 @@ echo "complete the clean"
 # git pull
 
 
-flags="-X '$path.app_version=v1.0' -X '$path.go_version=$(go version)' -X '$path.build_time=$(date "+%Y.%m.%d %H:%M:%S")' -X '$path.build_user=$(id -u -n)' -X '$path.commit_id=$(git rev-parse --short HEAD)'"
+flags="-X '${path}.app_version=v1.0' -X '${path}.go_version=$(go version)' -X '${path}.build_time=$(date "+%Y.%m.%d %H:%M:%S")' -X '${path}.build_user=$(id -u -n)' -X '${path}.commit_id=$(git rev-parse --short HEAD)'"
 buildResult=`go build -ldflags "$flags" -o "${targetFile}" "$buildPkg"`
 echo "$flags"
 

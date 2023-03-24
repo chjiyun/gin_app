63128cc59451baeb67a3a1e42b803c15d1faec10
chjiyun <lijiyun615@163.com>
2023-03-24 22:38:45 +0800

    fix file

--- app/service/file.go
+++ app/service/file.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"gin_app/app/common"
+	"gin_app/app/common/myError"
 	"gin_app/app/model"
 	"gin_app/app/result"
 	"gin_app/app/util"
@@ -21,13 +22,12 @@ import (
 )
 
 // Upload 接收上传的文件
-func Upload(c *gin.Context) *result.Result {
-	r := result.New()
+func Upload(c *gin.Context) (*model.File, error) {
 	db := c.Value("DB").(*gorm.DB)
 
 	f, err := c.FormFile("file")
 	if err != nil {
-		return r.FailType(common.FileNotFound)
+		return nil, myError.NewET(common.FileNotFound)
 	}
 
 	ext := filepath.Ext(f.Filename)
@@ -36,7 +36,7 @@ func Upload(c *gin.Context) *result.Result {
 	defer mFile.Close()
 	mime, err := mimetype.DetectReader(mFile)
 	if err != nil {
-		return r.FailErr(err)
+		return nil, err
 	}
 	mType := mime.String()
 	var filetype string
@@ -53,13 +53,13 @@ func Upload(c *gin.Context) *result.Result {
 	dirname := filepath.Dir(sourcepath)
 	err = os.MkdirAll(dirname, 0666)
 	if err != nil {
-		return r.FailErr(err)
+		return nil, err
 	}
 
 	// Upload the file to specific dst.
 	err = c.SaveUploadedFile(f, sourcepath)
 	if err != nil {
-		return r.Fail("上传失败")
+		return nil, myError.New("文件保存失败")
 	}
 
 	file := model.File{
@@ -74,9 +74,9 @@ func Upload(c *gin.Context) *result.Result {
 	}
 	res := db.Create(&file)
 	if res.Error != nil {
-		return r.FailType(common.UnknownError)
+		return nil, myError.NewET(common.UnknownError)
 	}
-	return r
+	return &file, nil
 }
 
 // Download 下载文件

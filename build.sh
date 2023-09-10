397c31c43cac1155ffb3c3f78ecc154833ecc797
chjiyun <lijiyun615@163.com>
2023-09-10 19:23:01 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -20,8 +20,6 @@ port="8000"
 today=$(date "+%Y_%m_%d")
 # 日志存放路径
 logDir="/root/logs/${appName}"
-info_log="${logDir}/info.${today}.log"
-error_log="${logDir}/error.${today}.log"
 path="main"
 
 echo "----------------------"
@@ -29,8 +27,6 @@ echo "app name: ${appName}"
 echo "app dir: ${pwd}"
 echo "current branch: ${localBranch}"
 
-
-echo "start clean log file"
 
 if [ ! -d "$logDir" ]; then
 # 创建多级目录
@@ -42,30 +38,44 @@ m=$(date -d "-6 months" "+%s")
 
 echo "半年前的日期: $(date -d @${m} "+%Y-%m-%d")"
 
-index=1
-f=`ls ${logDir} -1 -c`
+#index=1
+#f=`ls ${logDir} -1 -c`
+#
+## 清理旧日志文件
+#for name in $f
+#do
+#  # echo "日志${index}：$name"
+#  dateStr=$(echo ${name} | grep -Eo "[[:digit:]]{4}-[[:digit:]]{2}-[[:digit:]]{2}")
+#  # echo $dateStr
+#  # 判断是否有效
+#  if date -d ${dateStr} > /dev/null 2>&1; then
+#    t1=`date -d "$dateStr" +%s`
+#
+#    if [ $t1 -lt $m ]; then
+#      echo ">>> delete file: ${name}"
+#      rm -rf "${logDir}/${name}"
+#    fi
+#
+#  fi
+#  let index++
+#done
 
-# 清理旧日志文件
-for name in $f
-do
-  # echo "日志${index}：$name"
-  dateStr=$(echo ${name} | grep -Eo "[[:digit:]]{4}-[[:digit:]]{2}-[[:digit:]]{2}")
-  # echo $dateStr
-  # 判断是否有效
-  if date -d ${dateStr} > /dev/null 2>&1; then
-    t1=`date -d "$dateStr" +%s`
-
-    if [ $t1 -lt $m ]; then
-      echo ">>> delete file: ${name}"
-      rm -rf "${logDir}/${name}"
+# 等待进程退出
+wait_for_process_exit() {
+  local pidKilled=$1
+  local begin=$(date +%s)
+  local end
+  while kill -0 "$pidKilled" > /dev/null 2>&1
+  do
+    echo -n "."
+    sleep 1;
+    end=$(date +%s)
+    if [ $((end-begin)) -gt 60  ];then
+        echo -e "\nTimeout"
+        break;
     fi
-
-  fi
-  let index++
-done
-
-echo "complete the clean"
-
+  done
+}
 
 flags="-X '${path}.AppVersion=v1.0' -X '${path}.GoVersion=$(go version | awk '{print $3 " " $4}')' -X '${path}.BuildTime=$(date "+%Y.%m.%d %H:%M:%S")' -X '${path}.BuildUser=$(id -u -n)' -X '${path}.CommitId=$(git rev-parse --short HEAD)'"
 buildResult=`go build -ldflags "$flags" -o "${targetFile}" "$buildPkg"`
@@ -75,11 +85,11 @@ if [ $? -eq 0 ]; then
   chmod 773 ${targetFile}
   echo "build success, filename: ${targetFile}"
 
-  pid=`ps -ef |grep $targetFile | grep -v grep|awk '{print $2}'`
+  pid=$(ps -ef |grep $targetFile | grep -v grep|awk '{print $2}')
   echo "current pid is $pid"
   if [ -n "$pid" ]; then
-    echo "Prepare to kill the process: ${pid}"
-    kill -9 $pid
+    echo "Try to kill the process: ${pid}"
+    kill $pid && wait_for_process_exit "$pid"
     sleep 1
   fi
 else

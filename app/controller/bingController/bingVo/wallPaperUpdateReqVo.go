117382a7670bfb145a5f38b45512237ae21eb2fb
chjiyun <lijiyun615@163.com>
2023-10-15 20:01:05 +0800

    fix

--- app/controller/bingController/bingVo/wallPaperUpdateReqVo.go
+++ app/controller/bingController/bingVo/wallPaperUpdateReqVo.go
@@ -6,8 +6,8 @@ import (
 
 // 接收post form参数
 type WallPaperUpdateReqVo struct {
-	ID        string     `json:"id" binding:"required"`
-	FileId    string     `json:"fileId"`
+	ID        string     `form:"id" json:"id" binding:"required"`
+	FileId    string     `form:"file_id" json:"file_id"`
 	Desc      string     `form:"desc" json:"desc"`
 	ReleaseAt *time.Time `form:"releaseAt" json:"releaseAt"`
 }
